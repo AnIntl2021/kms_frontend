9087d81b7e59733d9ad031a5b3b7e199203c44d7
Affan Kaskar <affanabbas89@gmail.com>
2026-05-11 18:40:15 +0300

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,22 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    sourcemap: false,
+    chunkSizeWarningLimit: 1000,
+    minify: 'esbuild',
+    rollupOptions: {
+      output: {
+        manualChunks: {
+          'lucide': ['lucide-react'],
+          'vendor': ['react', 'react-dom', 'react-router-dom', 'axios', 'zustand'],
+        },
+      },
+    },
+  },
   server: {
     port: 5175,
     host: true
